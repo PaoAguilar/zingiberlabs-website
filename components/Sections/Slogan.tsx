c5e0f28ebfccca6477470fa8afbff3e58b4c0706
Paola Aguilar <pao.aquev29@gmail.com>
2023-08-17 11:47:27 -0600

    remove margin styles

--- components/Sections/Slogan.tsx
+++ components/Sections/Slogan.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Slogan = () => {
   return (
-    <section aria-label="Slogan">
+    <section style={{ margin: 0 }}>
       <p>
         With ZingiberLabs, anything is possible and the possibilities are
         limitless. Are you in?
